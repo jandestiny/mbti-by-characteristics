8b5cbd857d9d6a65ee78e67cdd07f3ad08026085
jandestiny <jandestinyeze@gmail.com>
2020-08-17 16:00:56 +0200

    added first floating test and MBTI data

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
-
+var mbti_data = [
+                    {
+                        type : "ISTJ",
+                        characteristics : ["Responsible", "Sincere", "Analytical", "Reserved", "Realistic", "Systematic"],
+                        description : "Hardworking and trustworthy with sound practical judgement."
+                    },
+                    {
+                        type : "ISFJ",
+                        chracteristics : ["Warm", "Considerate", "Gentle", "Responsible", "Pragmatic", "Thorough"],
+                        description : "Devoted caretakers who enjoy being helpful to others."
+                    },
+                    {
+                        type : "INFJ",
+                        chracteristics : ["Idealistic", "Organized", "Insightful", "Dependable", "Compassionate", "Gentle"],
+                        description : "Seek harmony and cooperation, enjoy intellectual stimulation."
+                    },
+                    {
+                        type : "INTJ",
+                        chracteristics : ["Innovative", "Independant", "Strategic", "Logical", "Reserved", "Insightful"],
+                        description : "Driven by their own original ideas to achieve improvements."
+                    },
+                    {
+                        type : "ISTP",
+                        chracteristics : ["Action-oriented", "Logical", "Analytical", "Spontaneous", "Reserved", "Independent"],
+                        description : "Enjoy adventure, skilled at understanding how mechanical things work."
+                    },
+                    {
+                        type : "ISFP",
+                        chracteristics : ["Gentle", "Sensitive", "Nurturing", "Helpful", "Flexible", "Realistic"],
+                        description : "Seek to create a personal environment that is both beautiful and practical."
+                    },
+                    {
+                        type : "INFP",
+                        chracteristics : ["Sensitive", "Creative", "Idealistic", "Perceptive", "Caring", "Loyal"],
+                        description : "Value inner harmony and personal growth, focus on dreams and possibilities."
+                    },
+                    {
+                        type : "INTP",
+                        chracteristics : ["Intellectual", "Logical", "Precise", "Reserved", "Flexible", "Imaginative"],
+                        description : "Original thinkers who enjoy speculation and creative problem solving"
+                    },
+                    {
+                        type : "ESTP",
+                        chracteristics : ["Outgoing", "Realistic", "Action-oriented", "Curious", "Versatile", "Spontaneous"],
+                        description : "Pragmatic problem solvers and skillful negotiators."
+                    },
+                    {
+                        type : "ESFP",
+                        chracteristics : ["Playful", "Enthusiastic", "Friendly", "Spontaneous", "Tactful", "Flexible"],
+                        description : "Have strong common sense, enjoy helping people in tangible ways."
+                    },
+                    {
+                        type : "ENFP",
+                        chracteristics : ["Enthusiastic", "Creative", "Spontaneous", "Optimistic", "Supportive", "Playful"],
+                        description : "Value inspiration, enjoy starting new projects, see potential in others."
+                    },
+                    {
+                        type : "ENTP",
+                        chracteristics : ["Inventive", "Enthusiastic", "Strategic", "Enterprising", "Inquisitive", "Versatile"],
+                        description : "Enjoy new ideas and challenges, value inspiration."
+                    },
+                    {
+                        type : "ESTJ",
+                        chracteristics : ["Efficient", "Outgoing", "Analytical", "Systematic", "Dependable", "Realistic"],
+                        description : "Like to run the show and get things done in an orderly fashion."
+                    },
+                    {
+                        type : "ESFJ",
+                        chracteristics : ["Friendly", "Outgoing", "Reliable", "Conscientious", "Organized", "Practical"],
+                        description : "Seek to be helpful and please others, enjoy being active and productive"
+                    },
+                    {
+                        type : "ENFJ",
+                        chracteristics : ["Caring", "Enthusiastic", "Idealistic", "Organized", "Diplomatic", "Responsible"],
+                        description : "Skilled communicators who value connection with people."
+                    },
+                    {
+                        type : "ENTJ",
+                        chracteristics : ["Strategic", "Logical", "Efficient", "Outgoing", "Ambitious", "Independent"],
+                        description : "Effective organizers of people and long-range planners."
+                    }
+                ]
